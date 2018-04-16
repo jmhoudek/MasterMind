eced345aa18a316899c788ebdac5fd85ab015a9c
CHARLES STOCK <761825@ehs-s-232-23.local>
2018-04-16 12:40:44 -0500

    yippee

--- MasterMind App/AppDelegate.swift
+++ MasterMind App/AppDelegate.swift
@@ -12,7 +12,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var enableAllOrientation = false
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,12 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        if (enableAllOrientation == true){
-            return UIInterfaceOrientationMask.allButUpsideDown
-        }
-        return UIInterfaceOrientationMask.landscape
-    }
-
+ 
 }
 
